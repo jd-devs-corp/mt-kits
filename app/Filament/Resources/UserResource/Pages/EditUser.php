ad20746c0df7933fe1cefdc4755685b2ea3cb4a1
Darwin Fotso <fotsodarwin@gmail.com>
2024-02-27 18:28:16 +0000

    Update EditUser.php

--- app/Filament/Resources/UserResource/Pages/EditUser.php
+++ app/Filament/Resources/UserResource/Pages/EditUser.php
@@ -10,6 +10,8 @@ class EditUser extends EditRecord
 {
     protected static string $resource = UserResource::class;
 
+    protected static ?string $title = 'Modifier Utilisateur';
+
     protected function getHeaderActions(): array
     {
         return [
