cf8df4b0b9b88df1c7b8bcb4d384d96b72722d4c
Darwin Fotso <fotsodarwin@gmail.com>
2024-02-27 18:34:58 +0000

    Update ClientResource.php

--- app/Filament/Resources/ClientResource.php
+++ app/Filament/Resources/ClientResource.php
@@ -14,32 +14,13 @@ use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
-use Filament\Infolists;
-use Filament\Infolists\Infolist;
-use Ysfkaya\FilamentPhoneInput\Forms\PhoneInput;
-use Ysfkaya\FilamentPhoneInput\PhoneInputNumberType;
-use Ysfkaya\FilamentPhoneInput\Tables\PhoneColumn;
-use Ysfkaya\FilamentPhoneInput\Infolists\PhoneEntry;
-
 class ClientResource extends Resource
 {
     protected static ?string $model = Client::class;
 
-    protected static ?string $navigationGroup = 'Extérieur';
+    protected static ?string $navigationGroup = 'Exterieur';
 
     protected static ?string $navigationIcon = 'heroicon-o-user-group';
-
-    public static function infolists(Infolist $infolist): Infolist
-    {
-        return $infolist
-            ->columns([
-                Infolists\Components\TextEntry::make('name'),
-                Tables\Columns\TextColumn::make('email'),
-                PhoneEntry::make('phone')
-                    ->displayFormat(PhoneInputNumberType::NATIONAL)
-                    ->countryColumn('phone_country'),
-            ]);
-    }
 
     public static function form(Form $form): Form
     {
@@ -52,9 +33,10 @@ class ClientResource extends Resource
                     ->email()
                     ->required()
                     ->maxLength(255),
-                PhoneInput::make('phone_number')
-                    ->countryStatePath('phone_country')
-                    ->defaultCountry('CM'),
+                Forms\Components\TextInput::make('phone_number')
+                    ->tel()
+                    ->required()
+                    ->numeric(),
             ]);
     }
 
@@ -66,9 +48,9 @@ class ClientResource extends Resource
                     ->searchable(),
                 Tables\Columns\TextColumn::make('email')
                     ->searchable(),
-                PhoneColumn::make('phone_number')
-                    ->displayFormat(PhoneInputNumberType::NATIONAL)
-                    ->countryColumn('phone_country'),
+                Tables\Columns\TextColumn::make('phone_number')
+                    ->numeric()
+                    ->sortable(),
                 Tables\Columns\TextColumn::make('created_at')
                     ->dateTime()
                     ->sortable()
@@ -100,10 +82,10 @@ class ClientResource extends Resource
         ];
     }
 
-    public static function getNavigationBadge(): ?string
-    {
-        return static::getModel()::count();
-    }
+//     public static function getNavigationBadge(): ?string
+// {
+//     return static::getModel()::count();
+// }
 
     public static function getPages(): array
     {
@@ -112,6 +94,136 @@ class ClientResource extends Resource
             'create' => Pages\CreateClient::route('/create'),
             'view' => Pages\ViewClient::route('/{record}'),
             'edit' => Pages\EditClient::route('/{record}/edit'),
+        ];
+    }
+}
+ app/Filament/Resources/KitResource.php  0 → 100644
++
+121
+−
+0
+
+Viewed
+Conflict: This file was added both in the source and target branches, but with different contents. You can 
+.
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Clusters\Settings;
+use App\Filament\Resources\KitResource\Pages;
+use App\Filament\Resources\KitResource\RelationManagers;
+use App\Models\Kit;
+use App\Models\User;
+use Filament\Facades\Filament;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Illuminate\Support\Facades\Auth;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class KitResource extends Resource
+{
+    protected static ?string $model = Kit::class;
+
+    protected static ?string $navigationGroup = 'Exterieur';
+
+    protected static ?string $navigationIcon = 'heroicon-o-wifi';
+
+    // protected static ?string $cluster = Settings::class;
+
+    public static function form(Form $form): Form
+    {
+        $user = Auth::user();
+        return $form
+            ->schema([
+                Forms\Components\Select::make('client_id')
+                    ->label('Proprietaire')
+                    ->relationship('client','name')
+                    ->searchable()
+                    ->preload()
+                    ->createOptionForm([
+                        Forms\Components\TextInput::make('name')
+                            ->label('nom')
+                            ->required()
+                            ->maxLength(255),
+                        Forms\Components\TextInput::make('email')
+                            ->email()
+                            ->label('Addresse E-mail')
+                            ->required()
+                            ->maxLength(255),
+                        Forms\Components\TextInput::make('phone_number')
+                            ->tel()
+                            ->label('Numero de telephone')
+                            ->required()
+                            ->numeric(),
+                    ])
+                    ->required(),
+                Forms\Components\Hidden::make('user_id')
+                    ->default($user->role == 'fournisseur' ? $user->id : null),
+                Forms\Components\TextInput::make('kit_number')
+                    ->required()
+                    ->label('Numero de kit')
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('localisation')
+                    ->required()
+                    ->label('Localisation')
+                    ->maxLength(255),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('client.name')
+                    ->label('Proprietaire')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('user.name')
+                    ->sortable()
+                    ->label('Fournisseur')
+                    ->searchable(),
+
+                Tables\Columns\TextColumn::make('kit_number')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('localisation')
+                    ->searchable(),
+
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+                // Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\ExportBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+            RelationManagers\ReabonnementsRelationManager::class,
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListKits::route('/'),
+            'create' => Pages\CreateKit::route('/create'),
+            'view' => Pages\ViewKit::route('/{record}'),
+            'edit' => Pages\EditKit::route('/{record}/edit'),
         ];
     }
 }
