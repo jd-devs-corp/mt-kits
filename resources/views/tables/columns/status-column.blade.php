e1c361a1130ae2610f25544fda37b42d98fc59df
Darwin Fotso <fotsodarwin@gmail.com>
2024-02-29 09:01:39 +0100

    gestion des statuts

--- resources/views/tables/columns/status-column.blade.php
+++ resources/views/tables/columns/status-column.blade.php
@@ -1,27 +1,56 @@
 <div>
     <input type="hidden" name="" value="{{$record = $getRecord()}}">
-    <input type="hidden" name="" value="{{$dateFinAbonnement = strtotime($record->date_fin_abonnement)}}">
     <input type="hidden" name="" value="{{$now = strtotime(now())}}">
 
-    @if($dateFinAbonnement < $now)
+    @if ($record->date_fin_abonnement)
+        <input type="hidden" name="" value="{{$dateFinAbonnement = strtotime($record->date_fin_abonnement)}}">
+        @if($dateFinAbonnement < $now)
 
-        <div class="badge" style="background-color: rgb(247, 76, 76)">
-            Expiré
-        </div>
+            <div class="badge" style="background-color: rgb(247, 76, 76)">
+                Expiré
+            </div>
 
-    @elseif($dateFinAbonnement - $now < 14 * 24 * 60 * 60)
+        @elseif($dateFinAbonnement - $now < 14 * 24 * 60 * 60)
 
-        <div class="badge" style="background-color: rgb(252, 252, 101)">
-            Attention
-        </div>
+            <div class="badge" style="background-color: rgb(252, 252, 101)">
+                Attention
+            </div>
 
+        @else
+
+            <div class="badge" style="background-color: rgb(88, 173, 88)">
+                Valide
+            </div>
+
+        @endif
     @else
+            <input type='hidden' {{$i = 0}}>
+            @foreach ($record->reabonnements as $reabonnement)
+            <input type='hidden' {{$i++}}>
+            @endforeach
 
-        <div class="badge" style="background-color: rgb(88, 173, 88)">
-            Valide
-        </div>
+        <input type="hidden" name="" value="{{$dateFinAbonnement = strtotime($record->reabonnements[$i-1]['date_fin_abonnement'])}}">
+        @if($dateFinAbonnement < $now)
 
+            <div class="badge" style="background-color: rgb(247, 76, 76)">
+                Expiré
+            </div>
+
+        @elseif($dateFinAbonnement - $now < 14 * 24 * 60 * 60)
+
+            <div class="badge" style="background-color: rgb(252, 252, 101)">
+                Attention
+            </div>
+
+        @else
+
+            <div class="badge" style="background-color: rgb(88, 173, 88)">
+                Valide
+            </div>
+
+        @endif
     @endif
+
 </div>
 <style>
     .badge{
