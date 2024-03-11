53b98f044d3f09694da614f69b8fc5c3fd4a1978
jiordiviera <jiordiviera+1@gmail.com>
2024-03-11 09:31:11 +0100

    Jiordi

--- app/Http/Controllers/ReceiptController.php
+++ app/Http/Controllers/ReceiptController.php
@@ -19,18 +19,18 @@ class ReceiptController extends Controller
         $user->update(['somme_a_percevoir' => 0]);
         return response()->streamDownload(function () use ($dompdf) {
             echo $dompdf->output();
-        }, "reçu-{$user->id}.pdf");
+        }, "reçu-".now()->format('d/m/y H:i').".pdf");
     }
     public function generateReceipt($id)
     {
         $abonnement = Reabonnement::findOrFail($id);
 
         // Génération du contenu du reçu PDF
-        $pdf = \App::make('dompdf.wrapper')->setPaper('a4', 'landscape');
+        $pdf = \App::make('dompdf.wrapper')->setPaper('A4', 'portrait');
         $pdf->loadView('receipts.show', ['abonnement' => $abonnement]);
 
         // Envoi du PDF à l'utilisateur
-        return $pdf->download('recu-' . $abonnement->id . '.pdf');
+        return $pdf->download('reçu-' . now()->format('d/m/y H:i') . '.pdf');
 
 
     }
--- resources/views/receipts/show.blade.php
+++ resources/views/receipts/show.blade.php
@@ -1,37 +1,118 @@
-<link rel="preconnect" href="https://fonts.bunny.net">
-<link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-<style>
-    *, ::after,::before {
-        font-family: 'Sora', sans-serif;
-    }
-</style>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reçu de paiement</title>
+    <style>
+        body {
+            font-family: 'Sora', sans-serif;
+            font-size: 16px;
+            margin: 20px;
+            color: #000000;
+        }
 
-<div class="container mx-auto p-4">
-    <h1 class="text-2xl font-figtree font-bold mb-4 text-center">Reçu</h1>
+        .container {
+            max-width: 800px;
+            margin: 0 auto;
+        }
+        h1{
+            font-size: 2rem;
+            text-decoration: underline;
+        }
+        h2{
+            font-size: 1.25rem;
+            text-decoration: underline;
+            text-align: center;
+        }
 
-    <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
-      <div class="bg-white p-4 rounded-md shadow-sm">
-        <h2 class="text-xl font-semibold font-figtree mb-2 ">Informations du client</h2>
-        <p class="mb-1">Nom du propriétaire: {{ $abonnement->kit->client->name }}</p>
-      </div>
+        .header {
+            background-color: #334155;
+            color: #fff;
+            padding: 10px;
+            text-align: center;
+        }
 
-      <div class="bg-white p-4 rounded-md shadow-sm">
-        <h2 class="text-xl font-semibold font-figtree mb-2">Informations du kit</h2>
-        <p class="mb-1">Numéro du kit: {{ $abonnement->kit->kit_number }}</p>
-      </div>
+        .header img {
+            height: 100px;
+        }
 
-      <div class="bg-white p-4 rounded-md shadow-sm">
-        <h2 class="text-xl font-semibold font-figtree mb-2">Informations d'abonnement</h2>
-        <p class="mb-1">Date de début: {{ $abonnement->date_abonnement }}</p>
-        <p class="mb-1">Date de fin: {{ $abonnement->date_fin_abonnement }}</p>
-        <p class="mb-1">Plan tarifaire: {{ $abonnement->plan_tarifaire }}</p>
-      </div>
+        .title {
+            font-size: 20px;
+            font-weight: bold;
+            margin-bottom: 10px;
+        }
 
-      @if($abonnement->kit->user)
-      <div class="bg-white p-4 rounded-md shadow-sm">
-        <h2 class="text-xl font-semibold font-figtree mb-2">Informations du fournisseur</h2>
-        <p class="mb-1">Nom du fournisseur: {{ $abonnement->kit->user->name }}</p>
-      </div>
-      @endif
+        .section {
+            margin-bottom: 20px;
+            padding: 10px;
+            border: 1px solid #D3DCE6;
+            border-radius: 5px;
+        }
+
+        .section-title {
+            font-size: 18px;
+            font-weight: bold;
+            margin-bottom: 5px;
+        }
+
+        .table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+
+        .table td, .table th {
+            padding: 5px;
+            border: 1px solid #D3DCE6;
+        }
+
+        .table th {
+            text-align: left;
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+    <div class="header">
+        <img src="{{ public_path().'/images/logo_admin.png'}}" alt="Logo">
+        <h1>Reçu de paiement</h1>
     </div>
-  </div>
+
+    <div class="section">
+        <h2>Informations du client</h2>
+        <p>Nom : {{ $abonnement->kit->client->name }}</p>
+    </div>
+
+    <div class="section">
+        <h2>Informations du kit</h2>
+        <p>Numéro du kit : {{ $abonnement->kit->kit_number }}</p>
+    </div>
+
+    <div class="section">
+        <h2>Informations d'abonnement</h2>
+        <table class="table">
+            <tr>
+                <th>Date de début</th>
+                <td>{{ $abonnement->date_abonnement }}</td>
+            </tr>
+            <tr>
+                <th>Date de fin</th>
+                <td>{{ $abonnement->date_fin_abonnement }}</td>
+            </tr>
+            <tr>
+                <th>Plan tarifaire</th>
+                <td>{{ $abonnement->plan_tarifaire }} XAF</td>
+            </tr>
+        </table>
+    </div>
+
+    @if($abonnement->kit->user)
+        <div class="section">
+            <h2>Informations du fournisseur</h2>
+            <p>Nom du fournisseur : {{ $abonnement->kit->user->name }}</p>
+        </div>
+    @endif
+</div>
+</body>
+</html>
+
