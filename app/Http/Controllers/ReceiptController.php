96bba33d8badccf8174973e6bc312f74663add2e
jiordiviera <jiordiviera+1@gmail.com>
2024-03-11 08:42:23 +0100

    Jiordi

--- app/Http/Controllers/ReceiptController.php
+++ app/Http/Controllers/ReceiptController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Reabonnement;
 use Illuminate\Http\Request;
 use Dompdf\Dompdf;
 use App\Models\User;
@@ -19,5 +20,18 @@ class ReceiptController extends Controller
         return response()->streamDownload(function () use ($dompdf) {
             echo $dompdf->output();
         }, "reçu-{$user->id}.pdf");
+    }
+    public function generateReceipt($id)
+    {
+        $abonnement = Reabonnement::findOrFail($id);
+
+        // Génération du contenu du reçu PDF
+        $pdf = \App::make('dompdf.wrapper')->setPaper('a4', 'landscape');
+        $pdf->loadView('receipts.show', ['abonnement' => $abonnement]);
+
+        // Envoi du PDF à l'utilisateur
+        return $pdf->download('recu-' . $abonnement->id . '.pdf');
+
+
     }
 }
